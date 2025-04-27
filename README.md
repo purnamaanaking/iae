1ebecdd177bf1387442ce7641d6ebc28871a7230
Purnama Anaking <purnama.anaking@gmail.com>
2025-04-28 01:45:24 +0700

    Update

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Konten:
 
-- RESTFul API Sederhana menggunakan Laravel & MySQL
+- Contoh RESTFul API sederhana menggunakan Laravel & MySQL.
+- Contoh komunikasi antar layanan secara langsung (service-to-service).
+- Contoh penerapan microservices dengan tools Docker, RabbitMQ, Kubernetes.
+- Contoh penerapan GraphQL dengan Hasura.
--- service-to-service/user-service/routes/api.php
+++ service-to-service/user-service/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/students', StudentController::class);
+Route::apiResource('/users', UserController::class);
