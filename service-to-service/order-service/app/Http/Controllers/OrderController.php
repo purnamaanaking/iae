aba7bfca63ab96cbcb19d62cca68a359e458d352
Purnama Anaking <purnama.anaking@gmail.com>
2025-04-28 03:24:00 +0700

    Update

--- service-to-service/order-service/app/Http/Controllers/OrderController.php
+++ service-to-service/order-service/app/Http/Controllers/OrderController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
@@ -12,5 +15,47 @@ class OrderController extends Controller
     {
         $orders = Order::all();
         return new OrderResource($orders, 'Success', 'List of orders');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'code' => 'required',
+            'product_uuid' => 'required',
+            'user_uuid' => 'required',
+            'status' => 'required',
+            'total_price' => 'required',
+            'quantity' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return new OrderResource(null, 'Failed', $validator->errors());
+        }
+
+        $data = $request->all();
+        $data['uuid'] = (string) Str::uuid();
+        $product = Order::create($data);
+
+        return new OrderResource($product, 'Success', 'Order created successfully');
+    }
+
+    public function show($uuid)
+    {
+        $order = Order::where('uuid', $uuid)->first();
+        if ($order) {
+            $data = $order->toArray();
+
+            // Get the product details (consume)
+            $productResponse = Http::get('http://127.0.0.1:8002/api/products/'.$order->product_uuid);
+            $data['product'] = $productResponse->json()['data'];
+
+            // Get the user details (consume)
+            $userResponse = Http::get('http://127.0.0.1:8003/api/users/'.$order->user_uuid);
+            $data['user'] = $userResponse->json()['data'];
+
+            return new OrderResource($data, 'Success', 'Order found');
+        } else {
+            return new OrderResource(null, 'Failed', 'Order not found');
+        }
     }
 }
